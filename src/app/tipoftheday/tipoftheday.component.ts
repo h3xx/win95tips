4e2192bf808fb9272dfd18c678b63d9bcabf8344
Dan Church <dchurch@g2planet.com>
2018-07-29 11:47:56 -0500

    Improve button functions, style

--- src/app/tipoftheday/tipoftheday.component.ts
+++ src/app/tipoftheday/tipoftheday.component.ts
@@ -9,6 +9,7 @@ import {
     styleUrls: ['./tipoftheday.component.scss']
 })
 export class TipofthedayComponent implements OnInit {
+    private github = 'https://github.com/h3xx/win95tips';
 
     msg: string;
 
@@ -16,12 +17,24 @@ export class TipofthedayComponent implements OnInit {
         private weirdMessageService: WeirdMessageService,
     ) { }
 
-    newMessage() {
-        this.msg = this.weirdMessageService.getNextMessage();
-    }
-
     ngOnInit() {
         this.msg = this.weirdMessageService.getRandomMessage();
+    }
+
+    close() {
+        this.msg = 'You can\'t escape.';
+    }
+
+    whatsNew() {
+        window.location.href = this.github;
+    }
+
+    onlineRegistration() {
+        window.location.href = this.github;
+    }
+
+    nextTip() {
+        this.msg = this.weirdMessageService.getNextMessage();
     }
 
 }
